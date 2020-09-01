3c4a7eb514a2d8eb74bb13f4d570667dafc6d282
Jonny Burger <jonathanburger11@gmail.com>
2020-09-01 12:35:48 +0200

    feat(lib): add code

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,80 @@
-export default {
-  multiply(a: number, b: number) {
-    return Promise.resolve(a * b);
-  },
+import React, { useMemo } from 'react';
+import {
+  GestureResponderEvent,
+  TouchableWithoutFeedback,
+  TouchableWithoutFeedbackProps,
+} from 'react-native';
+import { State, TapGestureHandler } from 'react-native-gesture-handler';
+import Animated, {
+  and,
+  call,
+  cond,
+  eq,
+  set,
+  sub,
+  useCode,
+} from 'react-native-reanimated';
+import {
+  useTapGestureHandler,
+  useValue,
+  withSpringTransition,
+} from 'react-native-redash';
+
+const AnimatedTouchableWithoutFeedback = Animated.createAnimatedComponent(
+  TouchableWithoutFeedback
+);
+
+export const TouchableScale: React.FC<
+  Omit<TouchableWithoutFeedbackProps, 'onPress'> & {
+    onPress?: (event: GestureResponderEvent) => void;
+  }
+> = ({ style: propStyle, children, onPress, disabled, ...props }) => {
+  const tapHandler = useTapGestureHandler();
+  const scaleFactor = useMemo(
+    () => cond(eq(tapHandler.state, State.BEGAN), 0.05, 0),
+    [tapHandler.state]
+  );
+
+  const style = useMemo(() => {
+    return [
+      propStyle,
+      disabled
+        ? {}
+        : {
+            transform: [{ scale: sub(1, withSpringTransition(scaleFactor)) }],
+          },
+    ];
+  }, [disabled, propStyle, scaleFactor]);
+
+  const lastState = useValue(State.UNDETERMINED);
+
+  useCode(
+    () => [
+      cond(
+        and(eq(lastState, State.BEGAN), eq(State.END, tapHandler.state)),
+        call([], () =>
+          disabled
+            ? undefined
+            : onPress?.((null as unknown) as GestureResponderEvent)
+        )
+      ),
+      set(lastState, tapHandler.state),
+    ],
+    [onPress, disabled]
+  );
+
+  return (
+    <TapGestureHandler
+      {...tapHandler.gestureHandler}
+      maxDurationMs={10000000}
+      maxDeltaX={100}
+      maxDeltaY={100}
+    >
+      <AnimatedTouchableWithoutFeedback {...props}>
+        <Animated.View style={style} pointerEvents="box-only">
+          {children}
+        </Animated.View>
+      </AnimatedTouchableWithoutFeedback>
+    </TapGestureHandler>
+  );
 };
